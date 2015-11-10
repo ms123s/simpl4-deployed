b125d68a49240c4a49aeeff946ee96467990294f
ms <manfred@ms123.org>
2015-11-10 22:52:24 +0100

    .

--- bin/setup.bat
+++ bin/setup.bat
@@ -8,11 +8,6 @@ IF not [%2] == [] GOTO Next1
 set P2=%cd%
 :Next1
 
-call :Strip %P2%
-goto :EOF
-:Strip
-set P2=%1
-
 setx SIMPL4DIR %P2% >nul
 set SIMPL4DIR=%P2%
 %P2%\bin\bash.exe %P2%\bin\setup.sh %P1% %P2% %P3% %P4%
