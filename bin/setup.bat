2fbde1091d355e6b69161a43e7ee987f9f457458
ms <manfred@ms123.org>
2015-11-10 22:28:17 +0100

    .

--- bin/setup.bat
+++ bin/setup.bat
@@ -1,5 +1,17 @@
 @echo off
-setx SIMPL4DIR %2
-set SIMPL4DIR=%2
-%2\bin\bash.exe %2\bin\setup.sh %1 %2 %3 %4 %5
+
+set CURDIR=%cd%	
+IF [%2] == [] GOTO Next1
+set CURDIR=%2%
+:Next1
+
+call :Strip %CURDIR%
+goto :EOF
+:Strip
+
+set CURDIR=%1
+
+setx SIMPL4DIR %CURDIR% >nul
+set SIMPL4DIR=%CURDIR%
+%CURDIR%\bin\bash.exe %CURDIR%\bin\setup.sh %1 %2 %3 %4 %5
 
