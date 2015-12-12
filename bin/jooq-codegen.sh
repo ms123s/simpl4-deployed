4c1575be2a184efe506f5785dbe20cab7e72c557
ms <manfred@ms123.org>
2015-12-12 06:21:47 +0100

    .

--- bin/jooq-codegen.sh
+++ bin/jooq-codegen.sh
@@ -22,19 +22,21 @@ done
 
 CONFIGFILE=
 BUILD=
+GEN=
 NAMESPACE=
+WORKSPACE=
 #########################################################
 # usage
 #########################################################
 usage() {
-   echo "usage: $0 [-b] -n namespace -c configfile.xml"
+   echo "usage: $0 [-w] [-b] [-g]  -n namespace [-c configfile.xml]"
 }
 
 #########################################################
 # parameter
 #########################################################
-shortoptions='n:c:b'
-longoptions='namespace:configfile:build'
+shortoptions='n:c:bw'
+longoptions='namespace:configfile:build:workspace'
 getopt=$(getopt -o $shortoptions --longoptions  $longoptions -- "$@")
 if [ $? != 0 ]; then
    usage
@@ -54,6 +56,14 @@ while true; do
          NAMESPACE=$1
          shift
       ;;
+      -w|--workspace)
+         WORKSPACE=true
+         shift
+      ;;
+      -g|--gen)
+         GEN=true
+         shift
+      ;;
       -b|--build)
          BUILD=true
          shift
@@ -64,20 +74,26 @@ while true; do
    esac
 done
 
-if [ -z "$CONFIGFILE" -o -z "$BUILD" -o -z $NAMESPACE ] ; then
+if [ -z "$NAMESPACE" ] ; then
    usage;
    exit 1
 fi
 
-BUILDIR=$SIMPL4DIR/workspace/jooq/build
-GENDIR=$SIMPL4DIR/workspace/jooq/gen
+if [ -n "$WORKSPACE" ] ; then
+	BUILDIR=$SIMPL4DIR/workspace/jooq/build
+	GENDIR=$SIMPL4DIR/workspace/jooq/gen
+else
+	BUILDIR=$SIMPL4DIR/gitrepos/$NAMESPACE/.etc/jooq/build
+	GENDIR=$SIMPL4DIR/gitrepos/$NAMESPACE/.etc/jooq/gen
+fi
+
 CONFIGFILE=$SIMPL4DIR/gitrepos/$NAMESPACE/.etc/jooq/$CONFIGFILE
 
 echo "BUILDIR:$BUILDIR"
 echo "GENDIR:$GENDIR"
 echo "CONFIGFILE=$CONFIGFILE"
 
-if [ -n "$CONFIGFILE" ] ; then
+if [ -n "$GEN" ] ; then
    echo "================="
    echo "generate ->"
    echo "================="
@@ -90,5 +106,5 @@ if [ -n "$BUILD" ] ; then
    echo "================="
    rm -rf $BUILDIR
    mkdir -p $BUILDIR
-   javac -d $BUILDIR -cp $CLASSPATH `find $GENDIR -name "*.java"`
+   javac -source 1.7 -target 1.7 -d $BUILDIR -cp $CLASSPATH `find $GENDIR -name "*.java"`
 fi
