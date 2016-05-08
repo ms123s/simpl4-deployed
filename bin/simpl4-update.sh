7aea3201122861d2d1823d82b8424340622faf30
ms <manfred@ms123.org>
2016-05-08 15:38:09 +0200

    .

---
+++ bin/simpl4-update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd /opt
+rm -rf simpl4_tmp
+simpl4/bin/start.sh stop
+git clone --depth=1 https://github.com/ms123s/simpl4-deployed.git simpl4_tmp
+cd simpl4_tmp
+tar cf - client server  | ( cd ../simpl4; tar xf -)
+cd /opt
+rm -rf simpl4_tmp
+simpl4/bin/start.sh start
+
